16eef391109155f3289ab1ea88d342e17f7d34f0
RobinEatsTheRich <rvasselt99@gmail.com>
2023-06-29 18:23:53 +0200

    broke something, will fix it soon, but end of the day now

--- src/pages/Cinema/Cinema.js
+++ src/pages/Cinema/Cinema.js
@@ -2,12 +2,20 @@ import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import {AccountContext} from "../../context/Account/AccountProvider";
 import axios from "axios";
+import {sleep} from "react-hook-form/dist/utils/sleep";
 
 function Cinema() {
     const { id } = useParams();
     const { tmdbKey, rapidKey } = useContext(AccountContext)
     const [ movieData, setMovieData ] = useState([])
     const [ reviewData, setReviewData ] = useState([])
+
+    //These are all the Reviews text bubbles
+    const [ textBubble1, setTextBubble1 ] = useState(<></>)
+    const [ textBubble2, setTextBubble2 ] = useState(<></>)
+    const [ textBubble3, setTextBubble3 ] = useState(<></>)
+    const [ textBubble4, setTextBubble4 ] = useState(<></>)
+
 
     useEffect(() => {
         const fetchOptionsTmdb = {
@@ -25,9 +33,13 @@ function Cinema() {
                 console.error(e);
             }
         }
-        const fetchOptionsRapid = {
+        void fetchMovieData();
+    }, [tmdbKey, id]);
+
+    useEffect(() => {
+        const fetchOptions = {
             method: 'GET',
-            url: `https://cinema-api.p.rapidapi.com/get_reviews/tt0118715`,
+            url: `https://cinema-api.p.rapidapi.com/get_reviews/${movieData.imdb_id}`,
             headers: {
                 'X-RapidAPI-Key': rapidKey,
                 'X-RapidAPI-Host': 'cinema-api.p.rapidapi.com'
@@ -35,37 +47,84 @@ function Cinema() {
         }
         async function fetchReviewData() {
             try {
-                const result = await axios.request(fetchOptionsRapid);
-                console.log(result)
+                const result = await axios.request(fetchOptions);
                 setReviewData(result.data.data);
             } catch (e) {
                 console.error(e);
             }
         }
 
-        void fetchMovieData();
-        void fetchReviewData()
-    }, [tmdbKey, rapidKey, id]);
+        void fetchReviewData();
+    }, [movieData.imdb_id, rapidKey, id]);
+
 
     useEffect(() => {
-        console.log("Movie Data:")
         console.log(movieData)
-
         console.log("Review Data:")
         console.log(reviewData)
-    }, [reviewData]);
+    }, [movieData, reviewData]);
 
+    function content() {
+        return(
+            <div className="review">
+                <p>
+                    {reviewData[
+                        Math.floor(Math.random() * reviewData.length)
+                        ].title}
+                </p>
+            </div>
+        )
+    }
+    async function summonReviews() {
+        let elementNumber = 0;
+        async function sleepTimer(){
+            await new Promise(r => setTimeout(r, (Math.random() * 10000 + 5000)));
+        }
+        while(elementNumber > 0) {
+            void sleepTimer()
+            if(elementNumber >= 4){
+                elementNumber = 0;
+            } else{
+                switch (elementNumber) {
+                    case 0:
+                        setTextBubble1(content())
+                        break;
+                    case 1:
+                        setTextBubble2(content())
+                        break;
+                    case 2:
+                        setTextBubble3(content())
+                        break;
+                    case 3:
+                        setTextBubble4(content())
+                        break;
+                    default:
+                        break;
+                }
+                elementNumber++
+            }
+        }
+    }
+
+    //getting the party started
+    useEffect(() => {
+        void summonReviews();
+    },[]);
 
     return (
         <>
             { movieData.original_title &&
-            <>
-                <h2>Now watching: {movieData.original_title}</h2>
+                <>
+                    <h2>Now watching: {movieData.original_title}</h2>
+                    {textBubble1}
+                    {textBubble2}
+                    {textBubble3}
+                    {textBubble4}
 
-            </>
+                </>
             }
         </>
     );
-};
+}
 
 export default Cinema;
